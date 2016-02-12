f2d6c7476f9b2ce51bef71fb9d8caa0b18dd1804
Pyroh <pyroh@icloud.com>
2016-02-12 15:09:14 +0100

    [ADD] Storyboard sample

---
+++ Samples/SampleStoryboard/SampleStoryboard/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SampleStoryboard
+//
+//  Created by Pierre TACCHI on 12/02/16.
+//  Copyright © 2016 Pyrolyse. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Samples/SampleStoryboard/SampleStoryboard/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SampleStoryboard
+//
+//  Created by Pierre TACCHI on 12/02/16.
+//  Copyright © 2016 Pyrolyse. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
