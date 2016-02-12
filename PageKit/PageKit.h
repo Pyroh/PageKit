67f47af18494c854ce69586039c3e819672406d8
Pyroh <pyroh@icloud.com>
2016-02-12 14:38:22 +0100

    Initial Commit

---
+++ PageKit/PageKit.h
@@ -0,0 +1,19 @@
+//
+//  PageKit.h
+//  PageKit
+//
+//  Created by Pierre TACCHI on 12/02/16.
+//  Copyright © 2016 Pyrolyse. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PageKit.
+FOUNDATION_EXPORT double PageKitVersionNumber;
+
+//! Project version string for PageKit.
+FOUNDATION_EXPORT const unsigned char PageKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PageKit/PublicHeader.h>
+
+
